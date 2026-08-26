3c2549a762e63d4b2bee971c0241d627d10bd67a
dezemberundjuli <webmaster@dezemberundjuli.ch>
2026-08-26 16:34:11 +0200

    Bump version to 0.1.2

--- elementor-breakout-sections.php
+++ elementor-breakout-sections.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Elementor Breakout Sections
  * Description: Bring back full-width breakout sections for Elementor Container layouts.
- * Version: 0.1.1
+ * Version: 0.1.2
  * Author: #dezemberundjuli
  * Author URI: https://dezemberundjuli.ch
  * License: MIT
